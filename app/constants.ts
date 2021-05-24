412f4ab88747e6961f8ae85de9c834a991faf271
danyare3 <danyare3@gmail.com>
2021-05-24 17:37:44 -0400

    se modifica el S/C

--- app/constants.ts
+++ app/constants.ts
@@ -1,7 +1,7 @@
 export const INFORMATION = {
     avatar: "/assets/avatar2.jpg ",
     banner: "/assets/banner3.jpg",
-    title:'Rapid FoodTruck S/c',
+    title:'Rapid FoodTruck S/C',
     description:'Lo Mejor de la Comida Rapida ',
     phone: `+584247380853`,
     shets:`https://docs.google.com/spreadsheets/d/e/2PACX-1vQGoIaHWRyYeqA7dZgt6TUuGf00oSjx5qrGJLa4sICBp5mRLIRQ9bIPMaxFTlnRtOgcLZRjMOAt0P7y/pub?output=csv`,
