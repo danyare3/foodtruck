e669df525717ed26c389f136b11393b4d893ac3e
danyare3 <danyare3@gmail.com>
2021-05-26 12:36:44 -0400

    banner1

--- app/constants.ts
+++ app/constants.ts
@@ -1,6 +1,6 @@
 export const INFORMATION = {
     avatar: "/assets/avatar2.jpg ",
-    banner: "/assets/banner2.jpg ",
+    banner: "/assets/banner1.jpg ",
     title:'Rapid FoodTruck S/c.',
     description:'Lo Mejor de la Comida Rapida ',
     phone: `+584247380853`,
